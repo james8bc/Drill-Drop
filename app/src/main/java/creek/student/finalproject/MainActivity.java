3543e7c03167018f3d445116f39eea29738c231b
SpaceManta <majoraman64@gmail.com>
2018-04-12 10:46:39 -0600

    Added main menu activity

---
+++ app/src/main/java/creek/student/finalproject/MainActivity.java
@@ -0,0 +1,32 @@
+package creek.student.finalproject;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+
+
+
+
+    }
+
+
+
+}
