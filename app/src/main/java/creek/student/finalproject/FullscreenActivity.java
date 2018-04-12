18fa4a74995229b79146e11fb5ee08167b92cbe8
brendan.creek89@gmail.com <brendan.creek89@gmail.com>
2018-04-12 10:39:14 -0600

    Basic Design

--- app/src/main/java/creek/student/finalproject/FullscreenActivity.java
+++ app/src/main/java/creek/student/finalproject/FullscreenActivity.java
@@ -1,12 +1,24 @@
 package creek.student.finalproject;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -14,9 +26,49 @@ import android.view.View;
  */
 public class FullscreenActivity extends AppCompatActivity {
 
+    private Handler mHandler;
+    private int level = 100;
+    private int start = 25;
+    private int count = 0;
+    private ImageView view;
+    private ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
+        view = (findViewById(R.id.imageView2));
+        constraintLayout = findViewById(R.id.constraintLayout);
+        mHandler = new Handler();
+        startRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                startTimer();
+                count++;
+            } finally {
+                if (count >= 100) stopRepeatingTask();
+                else mHandler.postDelayed(mStatusChecker, 1000 / level);
+            }
+        }
+    };
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
+    }
+
+    private void startTimer() {
+        start = start + 25;
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        constraintSet.connect(R.id.imageView2, ConstraintSet.TOP, R.id.constraintLayout, ConstraintSet.TOP, start);
+        constraintSet.applyTo(constraintLayout);
     }
 }
